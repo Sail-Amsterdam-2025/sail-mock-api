470cea2f98d411ed51a8b3b6b3bca0908f22daee
DewiCabret <89426007+DewiCabret@users.noreply.github.com>
2024-11-13 10:42:43 +0100

    .

--- Sail-MockApi/Sail-MockApi.Api/Controllers/ExampleController.cs
+++ Sail-MockApi/Sail-MockApi.Api/Controllers/ExampleController.cs
@@ -9,34 +9,38 @@ namespace Sail_MockApi.Api.Controllers
     {
         // GET: api/<ExampleController>
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IActionResult Get()
         {
-            return new string[] { "value1", "value2" };
+            string[] t = new string[] { "value1", "value2" };
+            return new OkObjectResult(t);
         }
 
         // GET api/<ExampleController>/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public IActionResult Get(int id)
         {
-            return "value";
+            return new OkObjectResult("value");
         }
 
         // POST api/<ExampleController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public IActionResult Post([FromBody] string value)
         {
+            return new OkObjectResult("value");
         }
 
         // PUT api/<ExampleController>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public IActionResult Put(int id, [FromBody] string value)
         {
+            return new OkObjectResult("value");
         }
 
         // DELETE api/<ExampleController>/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public IActionResult Delete(int id)
         {
+            return new OkObjectResult("value");
         }
     }
 }
