49f1d25941b93105ad714f01f5b051d2295ed34c
DewiCabret <89426007+DewiCabret@users.noreply.github.com>
2024-11-13 10:08:13 +0100

    init

---
+++ Sail-MockApi/start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# -d: detached mode
+# -p: port mapping
+# --name: container name
+# sail-mockapi:latest: image name
+# docker run -d -p 8080:8080 -p 8081:8081 --name sail-mockapi-container sail-mockapi:latest
+
+docker-compose up --build
